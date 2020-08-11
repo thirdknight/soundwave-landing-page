5bdd5ed8ef0767f61ee50913449d3ab2df8932e4
Edinson Ningles <edinson.ningles@gmail.com>
2020-08-11 11:48:53 +0200

    Revert "Revert "Add files to project""
    This reverts commit 356d590c751ee624e3f4db7d9330bf3300b3dc80.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+// Gulp v4
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+// const browserSync = require('browser-sync').create();
+
+//compile SCSS into CSS
+function style() {
+    // 1. where is my scss file
+    return gulp.src('./scss/**/*.scss')
+    // 2. pass that file through sass compiler
+    .pipe(sass().on('error', sass.logError))
+    // 3. where do I save the compiled CSS
+    .pipe(gulp.dest('./css'))
+    // 4. stream changes to all browser
+    // .pipe(browserSync.stream());
+}
+
+function watch() {
+    // browserSync.init({
+    //     server: {
+    //         baseDir: './'
+    //     }
+    // });
+    // gulp watch for scss, browserSync and js, comment what you don't need.
+    gulp.watch('./scss/**/*.scss', style); 
+    // gulp.watch('./*.html').on('change', browserSync.reload);
+    // gulp.watch('./js/**/*.js').on('change', browserSync.reload); 
+}
+
+exports.style = style;
+exports.watch = watch;
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include("includes/a_config.php");?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/styles.css">
+  <title>Document</title>
+</head>
+<body>
+<!-- <h1>Hello Wordl! .</h1>   -->
+<?php echo "<h1> Hello World! php </h1>"
+?>
+</body>
+</html>
